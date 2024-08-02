1c553d113cf3824c5583ecc205003ba0447b2c3c
Imran Nazeer S <149575738+ImranNazeerS@users.noreply.github.com>
2024-08-02 18:56:31 +0530

    Render : hosting

--- client/src/components/provider/FavoriteList.jsx
+++ client/src/components/provider/FavoriteList.jsx
@@ -22,37 +22,37 @@ export default function FavoriteList() {
   }, [currentUser, dispatch]);
 
   return (
-    <div className="min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <div className="h-52vh md:h-72vh bg-listbg bg-cover bg-center flex flex-col justify-center items-center text-white">
-        <div className="">
-          <h1 className="flex flex-col items-center text-2xl font-semibold text-zinc-600">
+        <div className="text-center">
+          <h1 className="md:text-2xl text-xl font-semibold text-zinc-600">
             Lists
           </h1>
-          <h1 className="flex flex-col items-center text-5xl font-bold mt-6  text-gray-700">
+          <h1 className="md:text-5xl text-3xl font-bold md:mt-4 text-gray-700">
             My Saved Lists
           </h1>
         </div>
       </div>
-      <div className="p-4 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {favorites === undefined || favorites === null ? (
-          <div>Loading...</div>
-        ) : favorites.filter(Boolean).length > 0 ? (
-          favorites.filter(Boolean).map((provider) => (
-            <ProviderItem
-              key={provider._id}
-              provider={{
-                ...provider,
-                totalBookings: null,
-              }}
-              isFavorite={true}
-            />
-          ))
+      <div>
+        {favorites.filter(Boolean).length > 0 ? (
+          <div className="p-4 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {favorites.filter(Boolean).map((provider) => (
+              <ProviderItem
+                key={provider._id}
+                provider={{
+                  ...provider,
+                  totalBookings: null,
+                }}
+                isFavorite={true}
+              />
+            ))}
+          </div>
         ) : (
-          <div className="flex flex-col mt-4">
-            <div className="flex flex-col items-center text-2xl font-semibold mb-6 text-zinc-600">
+          <div className="flex flex-col justify-center items-center mt-10">
+            <div className="text-2xl font-semibold mb-6 text-zinc-600 text-center">
               No Favorite Providers Not yet saved
             </div>
-            <div className="flex-col md:flex items-center text-2xl font-semibold mt-6 mb-6">
+            <div className="md:flex items-center text-2xl font-semibold mt-6 mb-6">
               <SearchBar />
             </div>
           </div>
