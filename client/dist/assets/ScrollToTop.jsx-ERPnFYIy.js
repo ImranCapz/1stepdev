3241cff38d1d74c6dcb73d464633fe5b5c7bb8ca
Imran Nazeer S <149575738+ImranNazeerS@users.noreply.github.com>
2024-08-12 15:03:35 +0530

    without dist

--- client/dist/assets/ScrollToTop.jsx-ERPnFYIy.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./react-DiIsFRGf.js";import{a as t}from"./react-router-Ck6p_Z-K.js";function s(){const{pathname:o}=t();return r.useEffect(()=>{window.scrollTo(0,0)},[o]),null}export{s as S};
--- client/vite.config.js
+++ client/vite.config.js
@@ -13,37 +13,4 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // Create separate chunks for each dependency in node_modules
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-          if (id.includes("src/pages")) {
-            // Create separate chunks for each page in the src/pages directory
-            return id
-              .toString()
-              .split("src/pages/")[1]
-              .split("/")[0]
-              .toString();
-          }
-          if (id.includes("src/components")) {
-            // Create separate chunks for each component in the src/components directory
-            return id
-              .toString()
-              .split("src/components/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
 });
